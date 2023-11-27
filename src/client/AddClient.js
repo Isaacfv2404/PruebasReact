d068265a46767352809a7198db4781689c598ca2
Crvst <cristopher.zunigazamora@ucr.ac.cr>
2023-11-27 17:41:39 -0600

    cssUpdate

--- src/client/AddClient.js
+++ src/client/AddClient.js
@@ -39,83 +39,87 @@ export default function AddClient() {
   };
 
   return (
-    <div className="container">
-      <h1 className="heading">Registrar Cliente</h1>
+    <div>
+      <link rel="stylesheet" href="/sale.css"></link>
+      <div className="container">
+        <h1 className="heading">Registrar Cliente</h1>
 
-      <form onSubmit={(e) => onSubmit(e)}>
-        <div className="form-group">
-          <label className="form-label">Nombre</label>
-          <input
-            type={'text'}
-            className="form-control"
-            placeholder="Ingrese su nombre"
-            name="name"
-            value={name}
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
-        <div className="form-group">
-          <label className="form-label">Apellidos</label>
-          <input
-            type={'text'}
-            className="form-control"
-            placeholder="Ingresa sus apellidos"
-            name="lastName"
-            value={lastName}
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <label className="form-label">Nombre</label>
+            <input
+              type={'text'}
+              className="form-control"
+              placeholder="Ingrese su nombre"
+              name="name"
+              value={name}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Apellidos</label>
+            <input
+              type={'text'}
+              className="form-control"
+              placeholder="Ingresa sus apellidos"
+              name="lastName"
+              value={lastName}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
 
-        <div className="form-group">
-          <label className="form-label">Identificación</label>
-          <input
-            type={'text'}
-            className="form-control"
-            placeholder="Ingresa el Número de Cédula"
-            name="identification"
-            value={identification}
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
+          <div className="form-group">
+            <label className="form-label">Identificación</label>
+            <input
+              type={'text'}
+              className="form-control"
+              placeholder="Ingresa el Número de Cédula"
+              name="identification"
+              value={identification}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
 
-        <div className="form-group">
-          <label className="form-label">Email</label>
-          <input
-            type={'text'}
-            className="form-control"
-            placeholder="Ingrese el correo"
-            name="email"
-            value={email}
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
-        <div className="form-group">
-          <label className="form-label">Dirección</label>
-          <input
-            type={'text'}
-            className="form-control"
-            placeholder="Ingrese la dirección"
-            name="address"
-            value={address}
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
-        <div className="form-group">
-          <label className="form-label">Teléfono</label>
-          <input
-            type={'text'}
-            className="form-control"
-            placeholder="Ingrese el número de teléfono"
-            name="phone"
-            value={phone}
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
+          <div className="form-group">
+            <label className="form-label">Email</label>
+            <input
+              type={'text'}
+              className="form-control"
+              placeholder="Ingrese el correo"
+              name="email"
+              value={email}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Dirección</label>
+            <input
+              type={'text'}
+              className="form-control"
+              placeholder="Ingrese la dirección"
+              name="address"
+              value={address}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Teléfono</label>
+            <input
+              type={'text'}
+              className="form-control"
+              placeholder="Ingrese el número de teléfono"
+              name="phone"
+              value={phone}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
 
-        <button className="submit-button" type="submit">
-          Enviar
-        </button>
-      </form>
+          <button className="submit-button" type="submit">
+            Enviar
+          </button>
+        </form>
+      </div>
     </div>
+
   );
 }
--- src/vehicle/AddVehicle.js
+++ src/vehicle/AddVehicle.js
@@ -64,80 +64,85 @@ export default function AddVehicle() {
 
 
   return (
-    <div className="container">
-      <h1 className="heading">Registrar Vehiculo</h1>
+    <div>
+      <link rel="stylesheet" href="/sale.css"></link>
 
-      <form onSubmit={(e) => onSubmit(e)}>
-        <div className="form-group">
-          <label className="form-label">Placa</label>
-          <input
-            type={'text'}
-            className="form-control"
-            placeholder="Ingrese el número de placa"
-            name="plate"
-            value={plate}
-            required="true"
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
-        <div className="form-group">
-          <label className="form-label">Marca</label>
-          <input
-            type={'text'}
-            className="form-control"
-            placeholder="Ingresa la marca"
-            name="brand"
-            value={brand}
-            onChange={(e) => onInputChange(e)}
-            required="true"
-          />
-        </div>
+      <div className="container">
+        <h1 className="heading">Registrar Vehiculo</h1>
 
-        <div className="form-group">
-          <label className="form-label">Modelo</label>
-          <input
-            type={'text'}
-            className="form-control"
-            placeholder="Ingresa el modelo"
-            name="model"
-            value={model}
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <label className="form-label">Placa</label>
+            <input
+              type={'text'}
+              className="form-control"
+              placeholder="Ingrese el número de placa"
+              name="plate"
+              value={plate}
+              required="true"
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Marca</label>
+            <input
+              type={'text'}
+              className="form-control"
+              placeholder="Ingresa la marca"
+              name="brand"
+              value={brand}
+              onChange={(e) => onInputChange(e)}
+              required="true"
+            />
+          </div>
 
-        <div className="form-group">
-          <label className="form-label">Año</label>
-          <input
-            type={'number'}
-            className="form-control"
-            placeholder="Ingrese el año del vehículo"
-            name="year"
-            value={year}
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
+          <div className="form-group">
+            <label className="form-label">Modelo</label>
+            <input
+              type={'text'}
+              className="form-control"
+              placeholder="Ingresa el modelo"
+              name="model"
+              value={model}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
 
-        <div className="form-group">
-          <label className="form-label">Nombre de Cliente Asociado</label>
-          <select
-            className="form-control"
-            name="clientId"
-            value={clientId}
-            onChange={(e) => onInputChange(e)}
-          >
-            <option value="">Selecciona un cliente</option>
-            {clients.map((client, index) => (
-              <option key={index} value={client.id}>
-                {client.name}
-              </option>
-            ))}
-          </select>
-        </div>
+          <div className="form-group">
+            <label className="form-label">Año</label>
+            <input
+              type={'number'}
+              className="form-control"
+              placeholder="Ingrese el año del vehículo"
+              name="year"
+              value={year}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
 
-        <button className="submit-button" type="submit">
-          Enviar
-        </button>
-      </form>
+          <div className="form-group">
+            <label className="form-label">Nombre de Cliente Asociado</label>
+            <select
+              className="form-control"
+              name="clientId"
+              value={clientId}
+              onChange={(e) => onInputChange(e)}
+            >
+              <option value="">Selecciona un cliente</option>
+              {clients.map((client, index) => (
+                <option key={index} value={client.id}>
+                  {client.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <button className="submit-button" type="submit">
+            Enviar
+          </button>
+        </form>
+      </div>
     </div>
+
   );
 }
